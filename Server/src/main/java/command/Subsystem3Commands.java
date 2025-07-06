aef9119d2b393b0815b869c5b965aca8a88407ef
Jovan Mosurović <mosurovicjovan@gmail.com>
2025-07-06 16:07:01 +0200

    Added command and needed files for subsystem3

---
+++ Server/src/main/java/command/Subsystem3Commands.java
@@ -0,0 +1,26 @@
+package command;
+
+public class Subsystem3Commands {
+    // Paket commands
+    public static final String CREATE_PAKET = "CREATE_PAKET";
+    public static final String UPDATE_PAKET_CENA = "UPDATE_PAKET_CENA";
+    public static final String GET_ALL_PAKETI = "GET_ALL_PAKETI";
+    
+    // Pretplata commands
+    public static final String CREATE_PRETPLATA = "CREATE_PRETPLATA";
+    public static final String GET_ALL_PRETPLATE_FOR_KORISNIK = "GET_ALL_PRETPLATE_FOR_KORISNIK";
+    
+    // Istorija slusanja commands
+    public static final String CREATE_SLUSANJE = "CREATE_SLUSANJE";
+    public static final String GET_ALL_SLUSANJA_FOR_AUDIO = "GET_ALL_SLUSANJA_FOR_AUDIO";
+    
+    // Omiljeni audio commands
+    public static final String ADD_OMILJENI_AUDIO = "ADD_OMILJENI_AUDIO";
+    public static final String GET_OMILJENI_AUDIO_FOR_KORISNIK = "GET_OMILJENI_AUDIO_FOR_KORISNIK";
+    
+    // Ocena commands
+    public static final String CREATE_OCENA = "CREATE_OCENA";
+    public static final String UPDATE_OCENA = "UPDATE_OCENA";
+    public static final String DELETE_OCENA = "DELETE_OCENA";
+    public static final String GET_ALL_OCENE_FOR_AUDIO = "GET_ALL_OCENE_FOR_AUDIO";
+}
